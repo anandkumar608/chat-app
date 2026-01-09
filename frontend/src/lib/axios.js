3d666fd6f51b2f65938087c71c547a6ebb06e84d
anandkumar608 <monibatu10@gmail.com>
2026-01-09 21:15:53 +0530

    fix axios baseurl to point to backend in production

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api",
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5000/api"
+      : "https://chat-app-euj2.onrender.com/api",
   withCredentials: true,
 });
